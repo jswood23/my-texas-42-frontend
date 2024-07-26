d44102cd532a1ceb5c98299a7a5b6d8be34398b9
jswood23 <56269953+jswood23@users.noreply.github.com>
2024-07-26 17:02:13 -0500

    update terminology page

--- src/pages/rules/rules-pages/terminology.tsx
+++ src/pages/rules/rules-pages/terminology.tsx
@@ -1,6 +1,8 @@
 
 import { Typography } from '@mui/material'
 import * as React from 'react'
+import Blank from '../suits/blank'
+import Six from '../suits/six'
 
 const TerminologyPage = () => {
   return (
@@ -9,22 +11,20 @@ const TerminologyPage = () => {
       <Typography className="paragraph">
         <ul>
           <li><b>Mark</b>: A point earned by winning a normal round. The first team to reach 7 marks wins the game.</li>
-          <li><b>Trick</b>: A set of four dominoes, each played by a different player.</li>
-          <li><b>Double</b>: A domino with the same number of dots on the top and bottom of the domino.</li>
-          <li><b>Count</b>: A domino that has dots equal to a multiple of 5 on its surface. Count dominoes alter the
-            amount of points a trick is worth.
-          </li>
-          <li><b>Suit</b>: A set of dots on either the upper or lower part of the domino. The highest suit on the first
-            domino played in a trick is the “suit” for the round.
-          </li>
-          <li><b>Trump</b>: The suit that is set as the highest suit in the round.</li>
+          <li><b>Trick</b>: A set of four dominoes, each played by a different player. Each trick is worth one point
+            within a round.</li>
+          <li><b>Double</b>: A domino with the same number of dots on both sides.</li>
+          <li><b>Count</b>: A domino whose dots sum up to a multiple of 5 on its surface. Each count domino is worth the
+            sum of its dots in points within the round.</li>
+          <li><b>Suit</b>: A set of dots on either the upper or lower part of the domino. Can be any number
+            between <Blank /> and <Six />.</li>
+          <li><b>Trump</b>: The suit that is chosen as the highest suit at the beginning of each round. This suit is decided
+           on by the winner of the bid.</li>
           <li><b>Hand</b>: The set of seven dominoes that is picked up by each player at the beginning of the round. Can
-            also refer to a “round” of dominoes.
-          </li>
-          <li><b>Table Talk</b>: Communication about bidding or hands between teammates. Giving information about your
-            hand to your teammate is illegal.
-          </li>
-          <li><b>Off</b>: A domino that is not guaranteed to win a trick.</li>
+            also refer to a “round” of dominoes.</li>
+          <li><b>Table Talk</b>: Communication about bidding or hands between teammates during a round. This behavior is
+            strictly prohibited.</li>
+          <li><b>Off</b>: A domino that will likely not win a trick.</li>
         </ul>
       </Typography>
     </>
