9b84ba602ae7f2ab82e1583f94da4c07086a45bc
jswood23 <56269953+jswood23@users.noreply.github.com>
2024-08-19 13:39:19 -0500

    update website title for staging

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import { StyleSheetManager } from 'styled-components'
 import RouterElements from './router'
+import React from 'react'
+import config from './constants/config'
 
 function App () {
+  React.useEffect(() => {
+    document.title = config.environment === 'production' ? 'MyTexas42' : 'Staging | MyTexas42'
+  }, [])
+
   return (
     <BrowserRouter>
       <StyleSheetManager shouldForwardProp={() => true}>
